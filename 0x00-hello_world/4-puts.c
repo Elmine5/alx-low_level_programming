2602ca0909dd693dbe46ddbeb6d8df018d8a1e1b
Elmine5 <sanchoelmine@gmail.com>
2023-09-07 11:56:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main -it prints out output using put
  * Return: always (0)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
